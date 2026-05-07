bb5a6a7163f9bf11d195aeac5d7a8911a9db4a6c
Mohamed El Oifi <eloifi.mohamed.17@gmail.com>
2026-05-07 03:48:44 -0400

    First commit

---
+++ docker-compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ============================================================
+# docker compose 
+# docker compose down
+# docker compose up
+# ============================================================
+
+docker compose down
+docker compose up --build
